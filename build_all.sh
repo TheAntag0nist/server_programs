20df63c628deeee829e269e3b14071cc41b55657
TheAntag0nist <teorus2314@yandex.ru>
2023-02-06 22:37:40 +0300

    devlog: delete some files

--- build_all.sh
+++
@@ -1,6 +0,0 @@
-cd _server_part/
-make rebuild
-cd ../_user_part/
-make rebuild
-cd ..
-
--- execute.sh
+++
@@ -1,7 +0,0 @@
-cd ./_server_part/
-gnome-terminal -- ./server_app
-sleep 1
-
-cd ../_user_part
-gnome-terminal -- ./client_app
-sleep 1
